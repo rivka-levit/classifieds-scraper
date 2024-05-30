ca05af8076ff4764b82c4e035ae2971cd1831afa
rivka-yuliia <juliacreadora@gmail.com>
2024-05-30 13:52:03 +0300

    linkextractor

--- classifieds/spiders/cars.py
+++ classifieds/spiders/cars.py
@@ -1,10 +1,26 @@
 import scrapy
 
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-class CarsSpider(scrapy.Spider):
+from classifieds.items import ClassifiedsItem
+
+
+class CarsSpider(CrawlSpider):
     name = "cars"
     allowed_domains = ["www.classifieds.co.zw"]
-    start_urls = ["https://www.classifieds.co.zw/zimbabwe-cars-vehicles"]
+    start_urls = ["https://www.classifieds.co.zw/zimbabwe-cars-vehicles/Toyota"]
+    rules = (
+        Rule(
+            LinkExtractor(
+                restrict_xpaths='//div[@class="listings"]'
+
+            ),
+            callback='parse',
+            follow=True
+        ),
+    )
 
     def parse(self, response, **kwargs):
         pass
