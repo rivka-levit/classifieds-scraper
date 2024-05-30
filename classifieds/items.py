86935835a1dc0bec1e5dd57314430941421ae074
rivka-yuliia <juliacreadora@gmail.com>
2024-05-30 13:40:18 +0300

    item model

--- classifieds/items.py
+++ classifieds/items.py
@@ -7,6 +7,7 @@ import scrapy
 
 
 class ClassifiedsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    price = scrapy.Field()
+    properties = scrapy.Field()
+    description = scrapy.Field()
