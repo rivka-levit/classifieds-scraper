34c87d05a8050385ab349f36e555565b89612cd3
rivka-yuliia <juliacreadora@gmail.com>
2024-05-31 15:56:48 +0300

    in out processors

--- classifieds/items.py
+++ classifieds/items.py
@@ -1,8 +1,27 @@
 import scrapy
 
+from itemloaders.processors import MapCompose, Join
+
+
+def clean(s):
+    return s[0].strip().replace('\t', '').replace('\n', '').replace(u'\xa0', u' ').strip('\t')
+
+
+def clean_prop(s):
+    return s.strip().replace('\t', '').replace('\n', '').replace(u'\xa0', u' ').strip('\t')
+
 
 class ClassifiedsItem(scrapy.Item):
-    title = scrapy.Field()
-    price = scrapy.Field()
-    properties = scrapy.Field()
-    description = scrapy.Field()
+    title = scrapy.Field(
+        output_processor=clean
+    )
+    price = scrapy.Field(
+        output_processor=clean
+    )
+    properties = scrapy.Field(
+        input_processor=MapCompose(clean_prop),
+        output_processor=Join(', ')
+    )
+    description = scrapy.Field(
+        output_processor=clean
+    )
--- classifieds/spiders/cars.py
+++ classifieds/spiders/cars.py
@@ -1,3 +1,5 @@
+import scrapy
+
 from scrapy.loader import ItemLoader
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -5,45 +7,50 @@ from scrapy.linkextractors import LinkExtractor
 from classifieds.items import ClassifiedsItem
 
 
-class CarsSpider(CrawlSpider):
+class CarsSpider(scrapy.Spider):
     name = "cars"
     allowed_domains = ["www.classifieds.co.zw"]
     start_urls = ["https://www.classifieds.co.zw/zimbabwe-cars-vehicles/Toyota"]
-    rules = (
-        Rule(
-            LinkExtractor(
-                restrict_xpaths='//div[@class="listings"]'
-
-            ),
-            callback='parse',
-            follow=True
-        ),
-    )
+    # rules = (
+    #     Rule(
+    #         LinkExtractor(
+    #             restrict_xpaths='//div[@class="listings"]'
+    #
+    #         ),
+    #         callback='parse',
+    #         follow=False
+    #     ),
+    # )
 
     def parse(self, response, **kwargs):
-        item = ItemLoader(
-            ClassifiedsItem(),
-            response=response,
-            selector=response
-        )
-        item.add_xpath(
-            'title',
-            '//div[contains(@class, "details")]/'
-            'h5[@class="listing-title"/a/text()]'
-        )
-        item.add_xpath(
-            'price',
-            '//div[contains(@class, "price")]/div[@class="amount"]'
-            '/div[contains(@class, "usd-price-tooltip")]/text()'
-        )
-        item.add_xpath(
-            'properties',
-            '//div[contains(@class, "properties")]/ul/li[@class="property"]'
-            '/text()'
-        )
-        item.add_xpath(
-            'description',
-            '//div[@class="line-clamp-3"]/p/text()'
-        )
+        gallery = response.xpath('//div[contains(@id, "listing-")]')
+        print(len(gallery))
 
-        yield item.load_item()
+        for listing in gallery:
+
+            item = ItemLoader(
+                ClassifiedsItem(),
+                response=response,
+                selector=listing
+            )
+            item.add_xpath(
+                'title',
+                './/div[contains(@class, "details")]/'
+                'h5[@class="listing-title"]/a/text()'
+            )
+            item.add_xpath(
+                'price',
+                '//div[contains(@class, "price")]/div[@class="amount"]'
+                '/div[contains(@class, "usd-price-tooltip")]/text()'
+            )
+            item.add_xpath(
+                'properties',
+                './/div[contains(@class, "properties")]/ul/li[@class="property"]'
+                '/text()'
+            )
+            item.add_xpath(
+                'description',
+                '//div[@class="line-clamp-3"]/p/text()'
+            )
+
+            yield item.load_item()
